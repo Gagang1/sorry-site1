54661b21f4569efac830e9a91f3d5fc24345f92e
Gagang1 <94596282+Gagang1@users.noreply.github.com>
2026-05-25 11:28:49 +0530

    all update

--- src/components/GiftPage.jsx
+++ src/components/GiftPage.jsx
@@ -203,7 +203,7 @@ export default function GiftPage() {
                       transition={{ duration: 0.8, delay: 0.6 }}
                       className="text-lg text-purple-200 leading-relaxed"
                     >
-                      Just like my love for you. I promise to cherish you every single day, and never take your
+                      Just like you. I promise to cherish you every single day, and never take your
                       beautiful heart for granted again.💖
                     </motion.p>
                     <motion.p
@@ -212,6 +212,8 @@ export default function GiftPage() {
                       transition={{ duration: 0.8, delay: 0.9 }}
                       className="text-xl text-pink-200"
                     >
+                      Yaar gussa mat rahiye naa aab 💕✨
+                      <bR></bR>
                       You mean everything to me💕✨
                     </motion.p>
                   </motion.div>
--- src/components/HugPage.jsx
+++ src/components/HugPage.jsx
@@ -104,7 +104,7 @@ export default function HugPage({ setCurrentPage }) {
           transition={{ duration: 0.8, delay: 1.1 }}
           className="text-xl text-purple-200 leading-relaxed"
         >
-          Just your arms around me, that’s all I want.💕
+          Just Aap yaar gussa ache nahi lagte, that’s all I want  .💕
         </motion.p>
       </motion.div>
 
